4a063a9d3bc67367c1008691244be5bef05d1f01
Jordan Smith <bear.chunda@gmail.com>
2018-04-25 01:35:19 -0500

    fix(app-controller) update resetGame action

--- app/components/minesweep-game/component.js
+++ app/components/minesweep-game/component.js
@@ -28,7 +28,10 @@ export default Component.extend({
 
   actions: {
     resetGame() {
-      get(this, 'resetGameAction')();
+      let difficulty = get(this, 'game.opts.difficulty');
+      let size = get(this, 'game.opts.width');
+
+      get(this, 'resetGameAction')(difficulty, size);
       set(this, 'timeSpent', 0);
     }
   }
