d2218cd593b34a8e894a6a043431300ff6afa754
Ryan LaBouve <imryanlabouve@gmail.com>
2018-04-25 00:54:39 -0500

    Adds color for neighbors

--- app/components/mf-square/component.js
+++ app/components/mf-square/component.js
@@ -1,11 +1,35 @@
 import Component from '@ember/component';
-import { get } from '@ember/object';
+import {get, computed} from '@ember/object';
 
 export default Component.extend({
   tagName: '',
 
   //passed in
   square: null,
+
+  color: computed('square.neighboringBombCount', function() {
+    let count = get(this, 'square.neighboringBombCount');
+    switch (count) {
+      case 0:
+        return '#bbbabb';
+      case 1:
+        return '#0022ED';
+      case 2:
+        return '#4A772B';
+      case 3:
+        return '#D3422A';
+      case 4:
+        return '#001371';
+      case 5:
+        return '#EFCF65';
+      case 6:
+        return '#91DD81';
+      case 7:
+        return '#5251B2';
+      case 8:
+        return '#bada55';
+    }
+  }).readOnly(),
 
   actions: {
     flagSquare(square, event) {
